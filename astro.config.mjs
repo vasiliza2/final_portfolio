5fb277efc615d8e9c1eff33e7d9857e60b123574
Elizaveta <elizavetavasileva332@gmail.com>
2025-12-22 13:42:18 +0300

    1

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  
+  site: 'https://vasiliza2.github.io', 
+  
+  
+  base: '/final_portfolio', 
+});
---
+++ src/config.ts
@@ -0,0 +1,138 @@
+export const siteConfig = {
+  name: "Елизаветы Васильевой",
+  title:"",
+  description: "Итоговое портфолио",
+  accentColor: "#d81d97ff",
+
+  social: {
+    email: "elizavetavasileva332@gmail.com",
+    github: "https://github.com/vasiliza2",
+  },
+  aboutMe:
+    "Имею опыт работы более 2-х лет в event сфере. На позиции менеджера управляла командой в процессе организации и проведения различных мероприятий и вела необходимую документацию. Получаю высшее образование по специальности «Разработка программного обеспечения», обладаю пониманием основ разработки и базовым пониманием проектного управления. Свободно читаю код на таких языках, как Python, C++, JavaScript и языки разметки. Одной из моих сильных сторон является умение работать в режиме многозадачности с большим объемом поступающей информации. Этот навык я приобрела благодаря ежедневному выполнению разных задач в режиме строгого дедлайна. Также моей сильной стороной является системное мышление: получая задачу, я умею разложить её на подзадачи, расставить приоритеты, оценить инструменты, источники информации, сроки работы и дальнейшие шаги по реализации. Позиционирую себя как открытого человека с хорошими навыками коммуникации с разными типами людей, я умею решать проблемы, которые могут возникать при общении. Могу добиваться результата, как работая в команде, так и выполняя самостоятельную работу.",
+  skills: ["Javascript", "React", "A/B test", "Python", "SQL", "Docker", "PowerBi"],
+  projects: [
+    {
+  name: "1 курс",
+  links: [
+    { label: "Информатика", url: "https://drive.google.com/drive/u/0/folders/1oNxZqabOsJIMZU2ktY8vjNvRiDFtwAqF" },
+    { label: "ОКА", url: "https://drive.google.com/drive/u/0/folders/1oWgly20YSI-o0jSORGitIA48GFcUu29q" },
+    { label: "ИТФ", url: "https://drive.google.com/drive/u/0/folders/1oWgly20YSI-o0jSORGitIA48GFcUu29q" },
+    { label: "ИТ в математике", url: "https://drive.google.com/drive/u/0/folders/1CaFqRD0wBrMyNY8YVobv5xo0AeDIHMDH" },
+    { label: "Дискретная математика", url: "https://drive.google.com/drive/u/0/folders/1-ASMxIqosVHjQsXx11tyVq8NLgFpwp_O" },
+    { label: "Матанализ", url: "https://drive.google.com/drive/u/0/folders/1-BnvbO8FhwJskL_4iJrpnFMITsgLFIbl" },
+    { label: "ИТ", url: "https://drive.google.com/drive/u/0/folders/1aNC2DXCAYRu4E0NitnrfmrwLwgWZVfxf" },
+    { label: "ЛАТМ", url: "https://drive.google.com/drive/u/0/folders/12xfmWFiur0-m-8U5Qnu3zIW0FaycUvRM" },
+    { label: "Программирование СИ", url: "https://drive.google.com/drive/u/0/folders/1O1BfqUuvfPL-ms3YnjdjSg7cu9RaNO6m" },
+    { label: "Практика", url: "https://github.com/vasiliza2/practice1" },
+  ],
+},
+    {
+  name: "2 курс",
+  links: [
+    {label:"Компьютерный практикум"},
+    {label:"Лабораторная работа №2", url:"https://vasiliza2.github.io/vasiliza2/"},
+    {label:"Лабораторная работа №3", url:"https://docs.google.com/document/d/14M29aSf0uBgO-x2W78Gnqff0iRb6F4Q489aWuT1IEMQ/edit?usp=sharing"},
+    {label:"Лабораторная работа №4", url:"https://drive.google.com/file/d/15bT-9mkWPAMRW-Upu5EgAziYeYD8FARf/view?usp=sharing"},
+    {label:"Лабораторная работа №5", url:"https://docs.google.com/document/d/1rj9eo3ibbkc7SxvAc7x4gtAMJ8nsnnhey4bTA87ZtZA/edit?usp=sharing"},
+    {label:"Лабораторная работа №6", url:"https://docs.google.com/document/d/1GrUOMVz3bYZJCCVXT_We2zzyIhv-9l5OD3S2axb-y8g/edit?usp=sharing"},
+    {label:"Лабораторная работа №7", url:"https://docs.google.com/document/d/1Xiw_nqd9l0GyMbxBotyCgefhXKMv9WL5EGUjhV-wyUw/edit?usp=sharing"},
+    {label:"Лабораторная работа №8", url:"https://drive.google.com/file/d/1Z3FOXUNv6qqHaOvDMl6KKC_Q3tJN3aj9/view?usp=sharing"},
+    {label:"Лабораторная работа №9.1", url:"https://docs.google.com/document/d/1cvBTsSUzU_y2H-lC2LhdzmpM3dFbuwjfTuZlJX6d6sU/edit?usp=sharing"},
+    {label:"Лабораторная работа №9.2", url:"https://vasiliza2.github.io/lektor-kp/"},
+    {label:""},
+    { label: "СВТ", url: "https://drive.google.com/file/d/1zMeM0J3E8v4aUOvQ0WLZk21jqI0HHL03/view?usp=sharing"},
+    {label:""},
+    {label:"Веб-проектирование"},
+    {label:"Лабораторная работа №1. Портфолио", url:"https://vasiliza2.github.io/vasiliza2/"},
+    {label:"Лабораторная работа №2. Сторонние сервисы", url:"https://adamen-blog.vercel.app/post/0"},
+    {label:"Лабораторная работа №3. Пользовательские компоненты", url:"https://adamen-blog.vercel.app/post/1"},
+    {label:"Лабораторная работа №4. Строки", url:"https://drive.google.com/file/d/1SJjvbcnBenSlda_gzg4DfxBAjH91scAM/view?usp=sharing"},
+    {label:""},
+    { label: "Базы данных", url: "https://replit.com/@elizavetavasil1/portfolioBD#Readme.md" },
+    { label: "Программирование", url: "https://replit.com/@elizavetavasil1/portfolioprogha34siem" },
+    { label: "Интегралы и ДУ", url: "https://drive.google.com/drive/folders/1-rCEHRylhYaJHgj5b8qu6uQL3QhoR-R7?usp=sharing" },
+    { label: "Английский язык", url: "https://drihttps://drive.google.com/drive/folders/1-qpZQW18DjU1P7A0sRyLRJEYx4sWEVVA?usp=sharing" },
+    { label: "Пакеты прикладных программ", url: "https://drive.google.com/drive/folders/1DLhI2BLZyb5WqWV4Nw-5ifnkRXOoJXaG?usp=sharing" },
+    { label: "Анализ данных", url: "https://drive.google.com/drive/folders/1uJ7KiDKQPooMlkhQf0BYoURB6rSAcvB4?usp=sharing" },
+    { label: "ТКМ", url: "https://drive.google.com/drive/folders/1-f819WQe4iJOTcL_bYEQ5V-4i8SjHYLF?usp=sharing" },
+    { label: "Вычислительная математика", url: "https://drive.google.com/drive/folders/1KTmYfft9mQiZMjVzAahX3o9kruW4_vaa?usp=sharing" },
+    { label: "Практика", url: "https://drive.google.com/drive/folders/19CHi7ZLlkXy7ToLI3Y1KwY1K8kUybn7-?usp=sharing" },
+  ],
+},
+{
+  name: "3 курс",
+  // ...
+  links: [
+    { label: "Английски язык", url: "https://drive.google.com/drive/folders/1XlWAwGeMTavJJLQs0s-gf9z15VoJuoEE?usp=sharing" },
+    { label: "Техническая документация", url: "https://drive.google.com/drive/folders/1LvqDwdeyGcB8gnpVpjDPWAq1a8VK28CU?usp=sharing" },
+    { label: "Сети и телекоммуникации", url: "https://drive.google.com/drive/folders/1SfXJ8rsywSqOk1N0XqX0yoigtVe191EI?usp=sharing" },
+    { label: "Публикация информации", url: "https://drive.google.com/drive/folders/1TIpojk9JVG5SfBrzgDwxlabdDE4GcdPj?usp=sharing" },
+    { label: "Программирование", url: "https://drive.google.com/drive/folders/1YnhOS4xMPa8uzwLaG8d3PKKdzSUifKAq?usp=sharing" },
+    { label: "Практика", url: "https://drive.google.com/drive/folders/1EE96m_ssbA6Gxz7oTo43eR6YfDOshenr?usp=sharing" },
+    { label: "ПИТ", url: "https://drive.google.com/drive/folders/18oJbjO2aGtL-r6I-rjV8rncfo4He2ilr?usp=sharing" },
+    { label: "Пакеты прикладных программ", url: "https://drive.google.com/drive/folders/14h4HhB5yyqjypjhdq2jkeEMixNc4x_1P?usp=sharing" },
+    { label: "ОЭУ", url: "https://drive.google.com/drive/folders/1eKusw5pZTTxdv7jwWQvwSxDENao-SdIC?usp=sharing" },
+    { label: "Основы корпоративного электронного обучения", url: "https://drive.google.com/drive/folders/1QuJZNZVjlX5aWTZfximky5c92bXfJiTc?usp=sharing" },
+    { label: "Основы компьютерной графики", url: "https://drive.google.com/drive/folders/1oAQNkaiuw3F93u3KZsaJksWLvOXoRBQr?usp=sharing" },
+    { label: "ИТиС", url: "https://drive.google.com/drive/folders/1k_WHfPjNyk2SpKgNPiH66igRAoa8skbc?usp=sharing" },
+    { label: "ИТ-менеджмент", url: "https://drive.google.com/drive/folders/1jxh00LEcnFY0dMYpj3pLiW5iOFeThQg_?usp=sharing" },
+    { label: "ИТ в иностанных языках", url: "https://drive.google.com/drive/folders/1WUJzqDjglNtnZpWJQkERpLgK5sbGvD_T?usp=sharing" },
+    { label: "Инженерная графика", url: "https://drive.google.com/drive/folders/1AgKpBdQ8TqFnoP1S6_h9u2NiDaBWt-3X?usp=sharing" },
+    { label: "Техники и технологии визуализации данных", url: "https://drive.google.com/drive/folders/1FG9RGZ4CTEwXZkKMNA9PDK5yMjp_ng2Y?usp=sharing" },
+    { label: "Бизнес аналитика", url: "https://drive.google.com/drive/folders/12KV1HeovbjEOd7gcVT8ZJO3dtR92WjIT?usp=sharing" },
+    { label: "Защита информации", url: "https://drive.google.com/drive/folders/1GJUO4wvyA827jzwvlYpVZIFc5rjfmcHt?usp=sharing" },
+    { label: "Математические основы глубокого обучения", url: "https://drive.google.com/drive/folders/1InZyHk-G3dHnPY7jUTD7tOKP2TY7-Xct?usp=sharing" },
+  ],
+},
+{
+  name: "4 курс (раздел в разработке)",
+  // ...
+  ///links: [
+    //{ label: "Ссылка 1", url: "https://url1.com" },
+    //{ label: "Ссылка 2", url: "https://url2.com" },
+  //],
+},
+  ],
+experience: [
+    {
+      title: "Дипломный проект МФТИ A/B-тестирование",
+      //company: "ООО Фитнес-мания",
+      //dateRange: "01.2023 — 06.2023",
+      link: "https://drive.google.com/drive/folders/14YeX4PabcV5OPgGqIMBWrLLwxXly5lWn?usp=sharing",
+      bullets: [
+        "Проведение A/B-тестирования для интернет-магазина «Фитнес-мания»",
+      ],
+    },
+    
+        {
+      title: "Интерактивный отчет в Power BI",
+      company: "",
+      dateRange: "",
+      link: "https://disk.yandex.ru/d/NqiXhne20OQ3Lw",
+      bullets: [
+        "анализ 1000 лучших фильмов с сайта Киноафиши",
+      ],
+    },
+  ],
+  education: [
+    {
+      school: "РГПУ им.Герцена",
+      degree: "Бакалавр",
+      dateRange: "2022 - 2026",
+      achievements: [
+        "09.03.01 Информатика и вычислительная техника",
+        "Технологии разработки программного обеспечения",
+      ],
+    },
+    {
+      school: "МФТИ",
+      degree: "Диплом профессиональной переподготовки",
+      dateRange: "2024-2025",
+      achievements: [
+        "Продуктовая аналитика",
+        "270 академических часов",
+      ],
+    },
+  ],
+};
