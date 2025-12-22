c2aa77cb2597b1fbef444ec429e2d73deab13d84
Elizaveta <elizavetavasileva332@gmail.com>
2025-12-22 14:00:46 +0300

    6

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  
-  site: 'https://vasiliza2.github.io', 
-  
-  
-  base: '/final_portfolio/', 
+  site: 'https://vasiliza2.github.io',
+  base: '/final_portfolio',
+  integrations: [tailwind({
+    applyBaseStyles: false, 
+  })],
 });
